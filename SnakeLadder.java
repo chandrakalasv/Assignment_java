fda3a13b0fcdc28bd5af650cd01d969081745b88
chandrakala <chadrakalasv1502@gmail.com>
2022-05-19 21:30:27 +0530

    added functinality to play snake and ladder till player reaches 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -7,22 +7,27 @@ public class SnakeLadder {
         int[] ladder_position = {14, 42, 68, 83, 95};
         int[] snake = {15, 38, 53, 69, 97};
         int[] bite_position = {4, 17, 54, 63, 16};
-
-        int dice = (int) ((Math.random() * 10) % 6) + 1;
+        while (player_position <= 100) {
+            int dice = (int) ((Math.random() * 10) % 6) + 1;
             System.out.println("dice number " + " " + dice);
-            System.out.println("initially player position is " + " " + player_position);
-        if (dice != 0) {
-            player_position = player_position + dice;
-            System.out.println("After dice roll player position is " + " " + player_position);
-            for (int i = 0; i < 5; i++) {
-                if (ladder[i] == player_position) {
-                    player_position = ladder_position[i];
-                    System.out.println("player gets ladder position" + " " + player_position);
-                } else if (snake[i] == player_position) {
-                    player_position = bite_position[i];
-                    System.out.println("player bitten by snake position" + " " + player_position);
+            System.out.println("player position is " + " " + player_position);
+            if (dice != 0) {
+                player_position = player_position + dice;
+                System.out.println("After dice roll player position is " + " " + player_position);
+                if (player_position == 100) {
+                    System.out.println("player gets winning position" + " " + player_position);
+                    break;
                 }
+                for (int i = 0; i < 5; i++) {
+                    if (ladder[i] == player_position) {
+                        player_position = ladder_position[i];
+                        System.out.println("player gets ladder position" + " " + player_position);
+                    } else if (snake[i] == player_position) {
+                        player_position = bite_position[i];
+                        System.out.println("player bitten by snake position" + " " + player_position);
+                    }
 
+                }
             }
         }
     }
