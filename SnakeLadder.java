7f7f7b9c2304e29e71a61e1bdd42593aeda91c33
chandrakala <chadrakalasv1502@gmail.com>
2022-05-19 14:16:09 +0530

    added code to generate random dice number between 1 to 6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,5 +3,6 @@ package com.bridge.day4;
 public class SnakeLadder {
     public static void main(String[] args) {
         int player_position = 0;
+        int dice = (int) ((Math.random() * 10) % 6) + 1;
     }
 }
